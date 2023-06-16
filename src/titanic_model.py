d1056727360507b240e9c3cbf7f8129c90f09975
Najir <irrperks@gmail.com>
2023-06-16 15:53:00 -0700

    Set up feature columns for estimators

--- src/titanic_model.py
+++ src/titanic_model.py
@@ -8,6 +8,10 @@ from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
 import tensorflow as tf
 
+#Used for feature columns later
+CAT_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUM_COLUMNS = ['age', 'fare']
+
 #One dataset is used for testing our data but a fresh set of data is needed for evaluating our model
 dfTrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
 dfEval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
@@ -37,4 +41,14 @@ trainClassPlot = dfTrain['class'].value_counts().plot(kind = "barh")
 #Recombines training data and survived values, groups them based on M/F
 #Creates the mean value of survived and sex, plots on bar graph, also sets custom label
 trainSexMeanPlot = pd.concat([dfTrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% Survived')
+
+featureColumn = []
+
+#Iterate on list of columns, append feature columns for each one in lists. Used for estimators
+for featureName in CAT_COLUMNS:
+    vocab = dfTrain[featureName].unique()
+    featureColumn.append(tf.feature_column.categrorical_column_with_vocabulary_lists(featureName, vocab))
+for featureName in NUM_COLUMNS:
+    featureColumn.append(tf.feature_column.numeric_column(featureName, dtype=tf.float32))
+
 
