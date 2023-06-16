016aa6532dc329860c233e38c947513a190ca9b6
Najir <irrperks@gmail.com>
2023-06-16 15:19:28 -0700

    Initial file creation, started adding basic data analysis and creating some graphs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tf-titanic-model
-Creating a linear regression model from Titanic dataset, using TensorFlow tutorial as a guide
+    Isaac Perks
+    06-16-2023
+
+# Description
+
+Following tutorial provided by: https://www.tensorflow.org/tutorials/estimator/linear
+This just serves as a point of practice with pandas, matplotlib, and tensorflow while using the provided tutorial as a guide on how to use tensorflow.
+
+- Creating a linear regression model based on Titanic dataset
+- Importing data via pandas to clean up and seperate information
+- Use numpy to efficiently process and manipulate arrays, vectors, and matrices
+- Using matplotlib to visualize some of the data and output information
+    - Creating and visualizing a lot of info on the dataset before testing the model
+    - Death statistics based on sex
+    - Grouping passengers by age and seperately by class as well
+- Tensorflow for the regression model and actual algorithm
---
+++ src/titanic_model.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from six.moves import urllib
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+#One dataset is used for testing our data but a fresh set of data is needed for evaluating our model
+dfTrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfEval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+
+#Seperating our given outputs to y and keeping data classifications inside df
+y_train = dfTrain.pop('survived')
+y_eval = dfEval.pop('survived')
+
+print(dfTrain.head()) #Simply print first 5 values in training dataset
+
+print(dfTrain.describe()) #A general list of statistical info based on the DF
+
+print(dfTrain.shape) #Shape of the matrix/vertex/arrays for our df
+
+#####
+# Graphs below need to be output
+#####
+
+trainAgeHist = dfTrain.age.hist(bins = 20) #Creates a histogram of DF age values with 20 seperate bins
+
+#Creates a bar graph of counts for male/female passengers
+trainSexPlot = dfTrain.sex.value_counts().plot(kind = 'barh')
+
+#Another bar graph showing the amount of passengers inside each class
+trainClassPlot = dfTrain['class'].value_counts().plot(kind = "barh")
+
+#Recombines training data and survived values, groups them based on M/F
+#Creates the mean value of survived and sex, plots on bar graph, also sets custom label
+trainSexMeanPlot = pd.concat([dfTrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% Survived')
+
